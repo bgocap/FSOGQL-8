47f80af6cac94e950438a062f46a17363f3b9d0b
bgocap <diego.capcha98@hotmail.com>
2024-03-05 16:57:28 +0200

    Exercise 8.2

--- library/index.js
+++ library/index.js
@@ -81,9 +81,18 @@ let books = [
 
 const typeDefs = `
 
+  type Book {
+    title:String!
+    author:String!
+    published:Int!
+    id:String!
+    genres:[String]!
+  }
+
   type Query {
     bookCount:Int!
     authorCount:Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -91,6 +100,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: (root, args) => books,
   },
 };
 
