11cb2119533d643bff62efc0c588098012363ad6
bgocap <diego.capcha98@hotmail.com>
2024-03-26 16:37:00 +0200

    Exercise 8.19

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Full Stack Web Development Open - Part 8</h1>
 
 GraphQL</br>
-Exercises are commits. Done **18** out of _26_
+Exercises are commits. Done **19** out of _26_
 </br></br>
 
 <h3>library</h3>
--- library-front/src/components/Books.jsx
+++ library-front/src/components/Books.jsx
@@ -1,33 +1,75 @@
+import { useEffect, useState } from "react";
 import { ALL_BOOKS } from "../services/queries";
 import { useQuery } from "@apollo/client";
 
+const Book = ({ book }) => (
+  <tr>
+    <td>{book.title}</td>
+    <td>{book.author.name}</td>
+    <td>{book.published}</td>
+  </tr>
+);
+
 const Books = () => {
   const result = useQuery(ALL_BOOKS);
-  console.log(result);
+  const [filter, setFilter] = useState(null);
+
   if (result.loading) {
     return <div>loading...</div>;
   }
+
   const books = result.data.allBooks;
-  //console.log(books);
+
+  const allGenres = books
+    .reduce((allGenresArray, book) => {
+      book.genres.map((genre) => allGenresArray.push(genre));
+      return allGenresArray;
+    }, [])
+    .filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+
   return (
     <div>
+      <div>
+        {filter ? <h3>In genre {filter}</h3> : <h3>All Genres</h3>}
+        <button
+          onClick={() => {
+            setFilter(null);
+          }}
+        >
+          All genres
+        </button>
+        {allGenres.map((genre) => (
+          <button
+            key={genre}
+            onClick={() => {
+              setFilter(genre);
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
       <h2>books</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>book</th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>book</th>
+              <th>author</th>
+              <th>published</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {filter
+              ? books
+                  .filter((book) =>
+                    book.genres.some((genre) => genre === filter)
+                  )
+                  .map((book) => <Book book={book} key={book.title}></Book>)
+              : books.map((book) => <Book book={book} key={book.title}></Book>)}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
